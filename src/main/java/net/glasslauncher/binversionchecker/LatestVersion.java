37652ec2bb0fbaf6d229aa689c7ff7c63ddd7862
calmilamsy <bumbill00@gmail.com>
2021-01-09 16:24:30 +0000

    Add simple printout and pop-up

---
+++ src/main/java/net/glasslauncher/binversionchecker/LatestVersion.java
@@ -0,0 +1,5 @@
+package net.glasslauncher.binversionchecker;
+
+public class LatestVersion {
+	public String sha;
+}
---
+++ src/main/java/net/glasslauncher/binversionchecker/OpenLinkWindow.java
@@ -0,0 +1,41 @@
+package net.glasslauncher.binversionchecker;
+
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.Font;
+
+public class OpenLinkWindow extends JOptionPane {
+
+    /**
+     * Used to make a pop-up clickable link.
+     * @param comp
+     * @param url
+     */
+    OpenLinkWindow(Component comp, String message, String url) {
+        JLabel label = new JLabel();
+        Font font = label.getFont();
+
+        String style = "font-family:" + font.getFamily() + ";" + "font-weight:" + (font.isBold() ? "bold" : "normal") + ";" +
+                "font-size:" + font.getSize() + "pt;";
+        JEditorPane link = new JEditorPane("text/html", "<html><body style=\"" + style + "\">"
+				+ "<p>" + message + "</p>"
+                + "<a href=\"" + url + "\">" + url + "</a>"
+                + "</body></html>");
+        link.addHyperlinkListener(event -> {
+            if (event.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+                try {
+                    Desktop.getDesktop().browse(event.getURL().toURI());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        link.setEditable(false);
+        link.setBackground(label.getBackground());
+        showMessageDialog(comp, link, "Outdated Mappings!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
